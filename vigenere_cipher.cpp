7d76a496c681bdf262ed8133654d0ad40658cad9
Saugat Gurung <163978570+grgsaugat11@users.noreply.github.com>
2025-08-04 12:15:40 +0545

    Update vigenere_cipher.cpp

--- vigenere_cipher.cpp
+++ vigenere_cipher.cpp
@@ -2,41 +2,50 @@
 #include<string.h>
 #include<ctype.h>
 
-int main(){
-	char msg[15];
-	char key[15];
-	printf("enter the message with less then length 16\n");
-	gets(msg);
-	printf("Enter the Key\n");
-	gets(key);
-	int msgLen = strlen(msg), keyLen = strlen(key),i,j;
-	for(int a = 0; msg[a]!='\0';a++){
-		msg[a]=toupper(msg[a]);
-	}
-	char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
-	for(i=0,j=0;i<msgLen;++i,++j){
-		if(j==keyLen)
-		j=0;
-		
-		newKey[i]=key[j];
-	}
-	newKey[i]='\0';
-	
-	for(i=0;i<msgLen;++i){
-		encryptedMsg[i]=((msg[i]+newKey[i])%26)+'A';
-	}
-	encryptedMsg[i]='\0';
-	
-	for(i=0;i<msgLen;++i){
-		decryptedMsg[i]=(((encryptedMsg[i]-newKey[i])+26)%26)+'A';
-	}
-	decryptedMsg[i]='\0';
-	
-	printf("Original Message: %s", msg);
-	printf("\nKey: %s", key);
-	printf("\nNew Generated Key: %s", newKey);
-	printf("\nEncrypted Message: %s", encryptedMsg);
-	printf("\nDecrypted Message: %s", decryptedMsg);
-	
-	return 0;
+int main() {
+    char msg[15];
+    char key[15];
+
+    printf("Enter the message (less than 16 characters):\n");
+    gets(msg);
+
+    printf("Enter the key:\n");
+    gets(key);
+
+    int msgLen = strlen(msg), keyLen = strlen(key), i, j;
+
+    for (i = 0; msg[i] != '\0'; i++) {
+        msg[i] = toupper(msg[i]);
+    }
+
+    for (i = 0; key[i] != '\0'; i++) {
+        key[i] = toupper(key[i]);
+    }
+
+    char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
+
+    for (i = 0, j = 0; i < msgLen; ++i, ++j) {
+        if (j == keyLen)
+            j = 0;
+        newKey[i] = key[j];
+    }
+    newKey[i] = '\0';
+
+    for (i = 0; i < msgLen; ++i) {
+        encryptedMsg[i] = ((msg[i] - 'A' + newKey[i] - 'A') % 26) + 'A';
+    }
+    encryptedMsg[i] = '\0';
+
+    for (i = 0; i < msgLen; ++i) {
+        decryptedMsg[i] = (((encryptedMsg[i] - 'A') - (newKey[i] - 'A') + 26) % 26) + 'A';
+    }
+    decryptedMsg[i] = '\0';
+
+    printf("Original Message: %s", msg);
+    printf("\nKey: %s", key);
+    printf("\nNew Generated Key: %s", newKey);
+    printf("\nEncrypted Message: %s", encryptedMsg);
+    printf("\nDecrypted Message: %s", decryptedMsg);
+
+    return 0;
 }
