43922815bff58eb320d8ed6c39ed31a1252a3a73
Saugat Gurung <163978570+grgsaugat11@users.noreply.github.com>
2025-08-05 11:35:08 +0545

    Update relationships.pl

--- relationships.pl
+++ relationships.pl
@@ -11,19 +11,20 @@ likes_food(Person):- likes(Person,_).
 same_food(Person1,Person2):- likes(Person1,Food),likes(Person2,Food),Person1\=Person2.
 
 #Queries
-a. likes cake but not pizza
-likes(X,cake),\+likes(X,pizza).
+# a. likes cake but not pizza
+# likes(X,cake),\+likes(X,pizza).
 
-b. does not like sushi
-likes(X,_),\+likes(X,sushi).
+# b. does not like sushi
+# likes(X,_),\+likes(X,sushi).
 
-c. how many people like pizza
-findall(X,likes(X,pizza),List),length(List,Count).
+# c. how many people like pizza
+# findall(X,likes(X,pizza),List),length(List,Count).
 
-d. uniquefood list
-setof(Food, X^likes(X,Food),UniqueFood).
+# d. uniquefood list
+# setof(Food, X^likes(X,Food),UniqueFood).
 
-e. pairs of people who like same food
-setof((Person1,Person2,Food),
-|    (likes(Person1,Food),likes(Person2,Food),Person1\=Person2),
-|    Pairs).
+# e. pairs of people who like same food
+# setof((Person1,Person2,Food),
+# |    (likes(Person1,Food),likes(Person2,Food),Person1\=Person2),
+# |    Pairs).
+
